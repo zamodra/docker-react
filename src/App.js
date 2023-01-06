b6fa58532dff2a2d365f9abe8b2b139afab36011
zamodra <odrassam@gmail.com>
2023-01-07 00:33:15 +0700

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was change on the feature branch
+          Learn React
         </a>
       </header>
     </div>
